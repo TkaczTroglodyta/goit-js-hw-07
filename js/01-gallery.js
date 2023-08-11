a44ae723f0bb6b9a867fa04b5efa85f51aae8a2f
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-11 22:30:29 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryBoxes = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+gallery.addEventListener('click', handleGalleryClick);
+
 console.log(galleryItems);
