354fda7e404b844f9e53b7c8d60a7e6d49e0f00b
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-08-12 19:51:35 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryBoxes = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+); // markup images into HTML page without 'data-source="${original}"'
+
+galleryBoxes.insertAdjacentHTML('beforeend', galleryMarkup.join('')); // inserting markup into <ul> tag
+
+let galleryImages = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
 console.log(galleryItems);
